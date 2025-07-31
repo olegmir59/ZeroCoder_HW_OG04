5ca0c02212e2812f366894ca1b26ca019a663340
olegmir59 <udom1@ya.ru>
2025-07-31 11:12:59 +0700

    add function quadrat(a,b)

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Это программадля подсчета площади прямоугольника")
+
+def quadrat(a,b):
+    if a > 0 and b > 0:
+        return a*b
+    else:
+        return "Ошибка"
+
+
+print(quadrat(23,7))
